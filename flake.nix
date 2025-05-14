7762c2cea061fc76ce4ee1596f940482bbac1ce7
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-05-14 10:01:13 +0100

    build with nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    zig.url = "github:mitchellh/zig-overlay";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, zig }:
+  outputs = { self, nixpkgs, zig-overlay }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
@@ -12,13 +12,31 @@
       devShells.x86_64-linux = {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            zig.packages."x86_64-linux"."0.14.0"
+            zig-overlay.packages."x86_64-linux"."0.14.0"
             libnotify
             glib
             gdk-pixbuf
             pkg-config
+
             valgrind
             zls
+          ];
+        };
+      };
+
+      packages.x86_64-linux = rec {
+        default = ntfyer;
+        ntfyer = pkgs.stdenvNoCC.mkDerivation {
+          name = "ntfyer";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            zig.hook
+            libnotify
+            glib
+            gdk-pixbuf
+            pkg-config
           ];
         };
       };
